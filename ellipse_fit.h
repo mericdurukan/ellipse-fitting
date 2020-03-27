6d2526b3f2cc330bdaf09337bf2e86d5f69429d4
mericdurukan <mericdurukan@gmail.com>
2020-03-27 07:46:06 -0700

    first commit

--- ellipse_fit.h
+++ ellipse_fit.h
@@ -16,13 +16,13 @@ class ellipse_fit{
 
 public:
 
-void set(vector<vector<double> > input);
-void fit(double& result_center_x, double& result_center_y, double& result_phi, double& result_width, double& result_hight);
+    void set(vector<vector<double> > input);
+    void fit(double& result_center_x, double& result_center_y, double& result_phi, double& result_width, double& result_hight);
 
 private:
 
-vector<vector<double> >input_matrix; 
-void take_input(vector<vector<double> > input);
+    vector<vector<double> >input_matrix;
+    void take_input(vector<vector<double> > input);
 
 };
 #endif
--- ellipse_main.cpp
+++ ellipse_main.cpp
@@ -4,38 +4,38 @@ using namespace std;
 
 int main(){
 
-vector<vector< double>> A;
-A.resize(11 , std::vector<double>(2, 0));
+    vector<vector< double>> A;
+    A.resize(11 , std::vector<double>(2, 0));
 
-for(int j =0; j<11; j++){
+    for(int j =0; j<11; j++){
 
-    A[j][0] = j;
-    A[j][1] = sqrt(400-j*j);
-
-
-}
-
-double center_x, center_y, phi, width, height;
-clock_t begin = clock();
-
-ellipse_fit my_ellipse;
-my_ellipse.set(A);
-my_ellipse.fit(center_x, center_y, phi, width, height);
-
-clock_t end = clock();
-double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-cout<<" elapsed time: "<<elapsed_secs *1000<<" ms"<<endl;
+        A[j][0] = j;
+        A[j][1] = sqrt(400-j*j);
 
 
+    }
 
-cout<<"center x: "<<center_x<<endl;
-cout<<"center y: "<<center_y<<endl;
-cout<<"phi: "<<phi<<endl;
-cout<<"width: "<<width<<endl;
-cout<<"height: "<<height<<endl;
+    double center_x, center_y, phi, width, height;
+    clock_t begin = clock();
+
+    ellipse_fit my_ellipse;
+    my_ellipse.set(A);
+    my_ellipse.fit(center_x, center_y, phi, width, height);
+
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout<<" elapsed time: "<<elapsed_secs *1000<<" ms"<<endl;
 
 
 
+    cout<<"center x: "<<center_x<<endl;
+    cout<<"center y: "<<center_y<<endl;
+    cout<<"phi: "<<phi<<endl;
+    cout<<"width: "<<width<<endl;
+    cout<<"height: "<<height<<endl;
 
-return 0;
+
+
+
+    return 0;
 }
